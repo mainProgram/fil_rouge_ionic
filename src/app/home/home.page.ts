e7117ab1d26d5889b634e35006914428368adb6b
mainProgram <fazeyfadiop@gmail.com>
2022-08-24 11:40:06 +0000

    styling bullets

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,14 @@ export class HomePage {
 
   public myImagePath = "https://images.unsplash.com/photo-1530554764233-e79e16c91d08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2VsY29tZSUyMGJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60";
 
+  public sliderOptions = {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true
+    }
+  };
+
   constructor() {}
 
 }
